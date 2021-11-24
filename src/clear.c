a73aa213dfaafd689abf1293cd68b8d6f718f4b4
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2021-11-24 20:34:23 +0100

    Clear.c updated
    Kłapouch dzięki jestem idiotą XD

--- src/clear.c
+++ src/clear.c
@@ -5,21 +5,16 @@
     #define OS_NAME "Windows"
 #elif _WIN64
     #define OS_NAME "Windows"
-#elif __linux__
-    #define OS_NAME "Linux"
-#elif TARGET_OS_MAC
-    #define OS_NAME "MacOS"
+#else
+    #define OS_NAME "OtherOS"
 #endif
 
 void cls() {
     if (OS_NAME == "Windows") {
         // If system is windows
         system("cls");
-    } else if (OS_NAME == "MacOS") {
-        // If system is macos
-        system("clear");
-    } else if (OS_NAME == "Linux") {
-        // If system is based on linux
+    } else if (OS_NAME == "OtherOS") {
+        // If system is macos or linux or any other system
         system("clear");
     }
 }
